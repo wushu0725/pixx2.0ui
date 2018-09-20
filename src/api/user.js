c03278f5f330e62fadd98c6d32c767ef55bf41f8
smallwei <1634566606@qq.com>
2018-09-20 17:16:44 +0800

    chomd(cli): close eslint

--- src/api/user.js
+++ src/api/user.js
@@ -18,40 +18,39 @@
 import request from '@/router/axios'
 
 export function fetchList(query) {
-  return request({
-    url: '/admin/user/userPage',
-    method: 'get',
-    params: query
-  })
+    return request({
+        url: '/admin/user/userPage',
+        method: 'get',
+        params: query
+    })
 }
 
 export function addObj(obj) {
-  return request({
-    url: '/admin/user/',
-    method: 'post',
-    data: obj
-  })
+    return request({
+        url: '/admin/user/',
+        method: 'post',
+        data: obj
+    })
 }
 
 export function getObj(id) {
-  return request({
-    url: '/admin/user/' + id,
-    method: 'get'
-  })
+    return request({
+        url: '/admin/user/' + id,
+        method: 'get'
+    })
 }
 
 export function delObj(id) {
-  return request({
-    url: '/admin/user/' + id,
-    method: 'delete'
-  })
+    return request({
+        url: '/admin/user/' + id,
+        method: 'delete'
+    })
 }
 
 export function putObj(obj) {
-  return request({
-    url: '/admin/user',
-    method: 'put',
-    data: obj
-  })
+    return request({
+        url: '/admin/user',
+        method: 'put',
+        data: obj
+    })
 }
-
