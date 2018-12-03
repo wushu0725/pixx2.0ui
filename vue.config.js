efae39a5fc5a54678c2cca76f5ca6dd1592804ef
smallwei <1634566606@qq.com>
2018-12-03 10:52:01 +0800

    fix styles

--- vue.config.js
+++ vue.config.js
@@ -1,70 +1,70 @@
-const url = 'http://localhost:9999'
+const url = 'http://zn.huaxiadaowei.com'
 module.exports = {
-    lintOnSave: true,
-    productionSourceMap: false,
-    chainWebpack: (config) => {
-        // 忽略的打包文件
-        config.externals({
-            'vue': 'Vue',
-            'vue-router': 'VueRouter',
-            'vuex': 'Vuex',
-            'axios': 'axios',
-            'element-ui': 'ELEMENT',
-        })
-    },
-    transpileDependencies: ['avue-plugin-transfer', 'avue-plugin-ueditor'],
-    // 配置转发代理
-    devServer: {
-        proxy: {
-            '/auth': {
-                target: url,
-                ws: true,
-                pathRewrite: {
-                    '^/auth': '/auth'
-                }
-            },
-            '/admin': {
-                target: url,
-                ws: true,
-                pathRewrite: {
-                    '^/admin': '/admin'
-                }
-            },
-            '/code': {
-                target: url,
-                ws: true,
-                pathRewrite: {
-                    '^/code': '/code'
-                }
-            },
-            '/gen': {
-                target: url,
-                ws: true,
-                pathRewrite: {
-                    '^/gen': '/gen'
-                }
-            },
-            '/daemon': {
-                target: url,
-                ws: true,
-                pathRewrite: {
-                    '^/daemon': '/daemon'
-                }
-            },
-            '/tx': {
-                target: url,
-                ws: true,
-                pathRewrite: {
-                    '^/tx': '/tx'
-                }
-            },
-            '/act': {
-                target: url,
-                ws: true,
-                pathRewrite: {
-                    '^/act': '/act'
-                }
-            },
+  lintOnSave: true,
+  productionSourceMap: false,
+  chainWebpack: config => {
+    // 忽略的打包文件
+    config.externals({
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios',
+      'element-ui': 'ELEMENT'
+    })
+  },
+  transpileDependencies: ['avue-plugin-transfer', 'avue-plugin-ueditor'],
+  // 配置转发代理
+  devServer: {
+    proxy: {
+      '/auth': {
+        target: url,
+        ws: true,
+        pathRewrite: {
+          '^/auth': '/auth'
         }
+      },
+      '/admin': {
+        target: url,
+        ws: true,
+        pathRewrite: {
+          '^/admin': '/admin'
+        }
+      },
+      '/code': {
+        target: url,
+        ws: true,
+        pathRewrite: {
+          '^/code': '/code'
+        }
+      },
+      '/gen': {
+        target: url,
+        ws: true,
+        pathRewrite: {
+          '^/gen': '/gen'
+        }
+      },
+      '/daemon': {
+        target: url,
+        ws: true,
+        pathRewrite: {
+          '^/daemon': '/daemon'
+        }
+      },
+      '/tx': {
+        target: url,
+        ws: true,
+        pathRewrite: {
+          '^/tx': '/tx'
+        }
+      },
+      '/act': {
+        target: url,
+        ws: true,
+        pathRewrite: {
+          '^/act': '/act'
+        }
+      }
     }
+  }
 }
